f19d79dbf797843b8dae6e23700bc959d5c14a9d
Soobin32 <166198846+Soobin32@users.noreply.github.com>
2025-03-10 21:33:31 +0800

    Update proxy.py

--- others/proxy.py
+++ others/proxy.py
@@ -121,5 +121,4 @@ def fetch_data():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or fallback to 5000 locally
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5001)))
